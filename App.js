287040a66c846d17d1eaa86e4255451d25230d5b
Animesh Dey <animesh.dey@weavers-web.com>
2023-10-16 17:04:09 +0530

    **

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import CalendarHeatmap from 'react-native-calendar-heatmap';
+
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <CalendarHeatmap
+        colorArray={["#D44B79", "#6B1928", "#9F3251", "#360000"]}
+        endDate={new Date('2016-01-01')}
+        numDays={365}
+        values={[
+          { date: '2016-01-01' },
+          { date: '2016-01-22' },
+          { date: '2016-01-30' },
+          // ...and so on
+        ]}
+      />
     </View>
   )
 }
