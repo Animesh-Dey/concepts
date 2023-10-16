7f023123445eabe36f9029cfd7ee083eba7309da
Animesh Dey <animesh.dey@weavers-web.com>
2023-10-16 16:42:44 +0530

    **

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Clustering from './concepts/clustering/Clustering'
 
 const App = () => {
   return (
-    <>
-      <Clustering />
-    </>
+    <View>
+      <Text>App</Text>
+    </View>
   )
 }
 
